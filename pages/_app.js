42d5ab412950726f633e15de923be881f43fdba8
Shreyas Dixit <84516626+SRDdev@users.noreply.github.com>
2025-01-12 21:48:49 +0530

    Removed Loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 import { useState } from 'react';
-import Loader from '../components/Loader';
+
 
 function MyApp({ Component, pageProps }) {
-  const [showLoader , setShowLoader] = useState(true);
-  if(showLoader){
-    return <Loader setShowLoader={setShowLoader} />
-  }
+  const [showLoader, setShowLoader] = useState(true);
   return (
     <>
       <Component {...pageProps} />
